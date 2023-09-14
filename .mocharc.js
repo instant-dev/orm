2511151e7b040f6ed7a87f4a83e696fba98f24ae
Keith Horwood <keithwhor@gmail.com>
2023-09-14 16:53:23 -0700

    Fix mocharc

--- .mocharc.js
+++ .mocharc.js
@@ -1,13 +1,1 @@
 process.env.npm_argv = JSON.stringify(process.argv.slice(0));
-
-(async () => {
-
-  return;
-
-  const Instant = require('...')();
-  await Instant.connect(cfg);
-  await Instant.loadSchema(url);
-
-  await Instant.connect(db, schema);
-
-})();
